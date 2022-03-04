77b95799fa5f388c1b7acdc29d636d87043716d9
albergvasili <albergvasili@yandex.com>
2022-03-04 18:41:10 -0500

    Finished developing Naraya chapter

--- adventure_game.py
+++ adventure_game.py
@@ -21,7 +21,7 @@ def intro():
 
 def valiant_question():
     valiant_explorer = input("'Are you the Valiant Explorer of the Soul?'"
-                             " (Y, N) ").lower()
+                             " (Y)/(N)").lower()
 
     if valiant_explorer == "y":
         printi("'Indeed! There is no time to waste...")
@@ -78,14 +78,13 @@ def valiant_question():
 
 printi("You follow the road to the main plaza.")
 printi("A young man is reading sitting by a tree nearby")
-printi("There is a tall statue of a woman holding a shiny object...")
+printi("There is a tall statue of a woman holding a small shiny object...")
 printi("It looks like a crystal.")
 printi("What would you like to do:")
 
 
 def plaza():
-    progress = ["Water"]
-    plaza_choice = input("Talk to young man. (T)\n"
+    plaza_choice = input("Approach young man. (Y)\n"
                          "Or steal the shiny object. (S)\n").lower()
 
     if plaza_choice == "s":
@@ -93,6 +92,7 @@ def plaza():
                "object is indeed a crystal.")
         printi("As you reach and grab the crystal, a guard sees you and "
                "alerts the town.")
+        progress.append("Crystal")
         printi("You put the crystal in your bag and quickly climb down.")
         printi("You run into an alley to avoid being seen")
         
@@ -125,9 +125,6 @@ def plaza():
         else:
             printi("Lelolelo") # add death ending
 
-   elif plaza_choice == "t":
-       printi("WHa")
-
         printi("As you get ready to proceed, you have a cold feeling "
                 "on your chest.")
         printi("An arrow pierced your heart. You feel the blood "
@@ -136,4 +133,88 @@ def plaza():
         printi("The moon gets close to the sun as your life gets " 
                "eclipsed by the darkness.")
 
+    elif plaza_choice == "Y":
+        printi("You walk towards the young man. You notice he is writing "
+               "some poetry.")
+        young_man_talk = input("Do you ask him about the crystal? (C)\n"
+                              "Or do you ask him about " 
+                              "his writings (W)\n").lower()
+
+        if young_man_talk == "c":
+            printi("'The crystal? Watch out stranger, don't let anybody "
+                   "hear you mentioning it.")
+            printi("It contains all the powers of this town.")
+            printi("It is said that the statue is cursed so it feels "
+                   "the power of the crystal on her hands.")
+            printi("I say the statue can only sense some weight, "
+                   " and that the cystal could be easily replaced "
+                   "with a small object... like that rock!'")
+            printi("'That's just me saying, anyways.'")
+            progress.append("Rock")
+            # define and call function to steal crystal
+        elif young_man_talk == "w":
+            printi("'Time")
+            printi("   the space of a thousand")
+            printi("Circles")
+            printi("   transitioning one by one\n")
+            printi("To eternally run")
+            printi("   away")
+            printi("From successive chains")
+            printi("To return")
+            printi("Time'\n")
+            printi("'What brings you to this town, stranger?'")
+            your_intention = input("Reveal your intentions about the "
+                                   "crystal (C)\n")
+                                   "Or you are just another wanderer "
+                                   "(W)\n").lower()
+            if your_intention = "c":
+                printi("'Oh noble stranger, let me help you in your quest.")
+                printi("For years I have observed this plaza.")
+                printi("I know how to avoid the spell!")
+                printi("Stay here, I will take the crystal and end all "
+                       "the magical nonsense that torments this town.'\n")
+                printi("The young man grabbed a rock and climbed the statue")
+                printi("He quickly swapped the crystal with the rock and "
+                       "puts it in his bag...")
+                printi("But three guards spot him, one of them alarms "
+                       "the town while the other two capture the young man")
+                printi("His bag falls to the ground while he struggles to "
+                       "escape.")
+                take_or_leave = input("Do you take the bag and run? (T)\n"
+                                      "Or do you help the young man? "
+                                      "(H)\n").lower()
+                
+                if take_or_leave = "t":
+                    printi("You take the bag and find a way out of the town.")
+                    progress.append("Bag")
+
+                elif take_or_leave = "h":
+                    printi("You push one of the guards, and the young man "
+                           "manages to escape.")
+                    printi("You try to follow him but some guards appear "
+                           "from an alley and capture you.\n")
+                    printi("In the prison, you keep grumbling about your "
+                           "decisions when you hear a murmur.")
+                    printi("The young man came to rescue you, and he "
+                           "managed to steal the keys of your cell.")
+                    printi("You escape the prison and leave the town "
+                           "without being spotted.")
+                    progress.append("Crystal")
+
+            elif your_intention == "w":
+                printi("'I will give you some good advice:'")
+                printi("'Advice'")
+                printi("The young man grabs his belongings and follow "
+                       "the main road.")
+                # Go back to plaza
+
+def Celadon():
+    printi("You are back at Celadon's tent")
+    if "Valiant" in progress:
+       crystal_master = input("'Valiant Explorer! Do you come back to "
+                              "offer the crystal to your master?'"
+                              "(Y)/(N)").lower()
+            
+ 
 plaza()
+
