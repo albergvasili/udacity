f413f3e696387181ae59588bc81eb6f0804ae2cb
albergvasili <albergvasili@yandex.com>
2022-03-20 10:45:14 -0400

    added method to announce winner

--- rps.py
+++ rps.py
@@ -1,29 +1,25 @@
 import random
 moves = ['rock', 'paper', 'scissors']
 
-class Player:
 
+class Player:
 
     def __init__(self):
         self.count = 0
 
-
     def move(self):
         return 'rock'
-
 
     def learn(self, my_move, their_move):
         pass
 
-
     def beats(one, two):
         return ((one == 'rock' and two == 'scissors') or
                 (one == 'scissors' and two == 'paper') or
-                (one == 'paper' and two == 'rock' ))
+                (one == 'paper' and two == 'rock'))
 
 
 class RandomPlayer(Player):
-
 
     def move(self):
         return random.choice(moves)
@@ -31,36 +27,46 @@ class RandomPlayer(Player):
 
 class HumanPlayer(Player):
 
-
     def move(self):
         self.moving = input("Rock, paper, scissors? > ").lower()
-        if self.moving in moves: 
-            return self.moving 
 
-        else:
-            self.move()
+        for options in moves:  # Why?
+
+            if self.moving in moves:
+                return self.moving
+
+            else:
+                self.move()
+
 
 class Game:
-
 
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
         self.round_count = 0
 
-
     def score_check(self, move1, move2):
         score = Player.beats(move1, move2)
 
-        if score == True:
+        if score == 1:
             self.p1.count += 1
 
-        elif score == False:
+        elif score == 0:
             self.p2.count += 1
 
         print("\tScore: " + str(self.p1.count) + " - " + str(self.p2.count)
               + "\n")
 
+    def winner(self):
+        if self.p1.count >= self.p2.count:
+            print("Player1 wins!") 
+
+        elif self.p1.count <= self.p2.count:
+            print("Player2 wins!")
+
+        else:
+            print("Tie")
 
     def play_round(self):
         self.round_count += 1
@@ -71,13 +77,13 @@ class Game:
         print(f"Player1: {move1}\tPlayer2: {move2}")
         self.score_check(move1, move2)
         self.p1.learn(move1, move2)
-        self.p2.learn(move2,move1)
-
+        self.p2.learn(move2, move1)
 
     def play_game(self):
         print("Game start!")
         for round in range(3):
             self.play_round()
+        self.winner()
         print("Game over!")
 
 
