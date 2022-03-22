0dd7ada1256e3b177200675e256d1bf59a1a6c3f
albergvasili <albergvasili@yandex.com>
2022-03-21 20:42:19 -0400

    define variables to start grid creating function

--- events.js
+++ events.js
@@ -1,2 +1,17 @@
-function(){
-}
+let designGrid = document.getElementById('designGrid');
+let gHeight = document.getElementById('gridHeight');
+let gWidth = document.getElementById('gridWidth');
+let forme = document.querySelector("table");
+let theButton = document.querySelector("button")
+
+theButton.addEventListener('click', function(){
+	mkgrid();
+	console.log("Ca marche");
+});
+
+
+function mkgrid() {
+		icell = document.createElement("tr");
+		forme.appendChild(icell);
+			
+};
