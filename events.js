fa6479213a58f1bd3f486c06bf9b910dc12fb053
albergvasili <albergvasili@yandex.com>
2022-03-22 17:41:34 -0400

    add function to change grid cell color

--- events.js
+++ events.js
@@ -1,21 +1,25 @@
 let designGrid = document.getElementById('designGrid');
 let gHeight = document.getElementById('gridHeight');
 let gWidth = document.getElementById('gridWidth');
-let theButton = document.querySelector("button")
+let theButton = document.querySelector('button')
 
 theButton.addEventListener('click', function(){
 	checkChild(designGrid);
-	console.log("Ca marche");
+	console.log('Ca marche');
 });
 
 
 function mkgrid() {
-	atable = document.createElement("table");
+	atable = document.createElement('table');
 	designGrid.appendChild(atable);
 	for (x = 0; x < gHeight.value; x++){
 		row = atable.insertRow(x);
 		for (y = 0; y < gWidth.value; y++){
-			row.insertCell(y);
+			let cellColor = row.insertCell(y);
+			cellColor.addEventListener('click', function(){
+				cellColor.style.background = 'blue';
+
+			});
 		}
 	}
 };
