afd4b1ba25ffb092a43942d7be26002de3d9f0aa
albergvasili <albergvasili@yandex.com>
2022-03-22 20:03:59 -0400

    comment functions and check indentation

--- events.js
+++ events.js
@@ -1,36 +1,48 @@
 let designGrid = document.getElementById('designGrid');
-let gHeight = document.getElementById('gridHeight');
-let gWidth = document.getElementById('gridWidth');
-let pColor = document.getElementById('pickColor');
+let gridHeight = document.getElementById('gridHeight');
+let gridWidth = document.getElementById('gridWidth');
+let pickColor = document.getElementById('pickColor');
 let theButton = document.querySelector('button');
 
-theButton.addEventListener('click', function(){
-	checkChild(designGrid);
+theButton.addEventListener('click', function() {
+    checkChild(designGrid);
 });
 
 
-function mkgrid() {
-	atable = document.createElement('table');
-	designGrid.appendChild(atable);
-	for (x = 0; x < gHeight.value; x++){
-		row = atable.insertRow(x);
-		for (y = 0; y < gWidth.value; y++){
-			let cellColor = row.insertCell(y);
-			cellColor.addEventListener('click', function(){
-				cellColor.style.background = pColor.value;
-			});
-		}
-	}
-};
+/** 
+ * mkgrid(): Create a grid of squares.
+ * Add table element to HTML,
+ * Insert rows using gridHeight value from input,
+ * insert cells on each row using gridWidth value from input,
+ * then change color of square when clicked on.
+ */
 
-function checkChild(element){
-	if (element.children.length >= 1){
-		while (element.firstChild) {
-			element.removeChild(element.firstChild);
-		}
-		mkgrid();
-	} else {
-		mkgrid();
-	}
+function mkgrid() {
+    createTable = document.createElement('table');
+    designGrid.appendChild(createTable);
+    for (x = 0; x < gridHeight.value; x++) {
+        row = createTable.insertRow(x);
+        for (y = 0; y < gridWidth.value; y++) {
+            let squareColor = row.insertCell(y);
+            squareColor.addEventListener('click', function() {
+                squareColor.style.background = pickColor.value;
+            });
+        }
+    }
+}
+
+/**
+ * checkChild(): Check element to determine whether an old table needs to be
+ * erased before creating a new one.
+ */
+
+function checkChild(element) {
+    if (element.children.length >= 1) {
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+        mkgrid();
+    } else {
+        mkgrid();
+    }
 };
-		
