fa54748ead1efdcb176e920854811d0b6a4dbcf7
albergvasili <albergvasili@yandex.com>
2022-04-15 19:35:20 -0400

    Add abstract base class 'IngestorInterface'

---
+++ QuoteEngine/IngestorInterface.py
@@ -0,0 +1,17 @@
+from .QuoteModule import QuoteModule
+from typing import List
+from abc import ABC, abstractmethod
+
+class IngestorInterface(ABC):
+    extensions=[]
+
+    @classmethod
+    def can_ingest(cls, path:str) -> bool:
+        ext = path.split('.')[-1]
+        return ext in extensions
+
+    @classmethod
+    @abstractmethod
+    def parse(cls, path:str) -> List[QuoteModule}:
+        pass
+
