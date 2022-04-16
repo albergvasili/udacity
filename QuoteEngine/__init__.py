53f3ef8305bd3b8bd5bbdb24d558dc66d4b284e2
albergvasili <albergvasili@yandex.com>
2022-04-16 19:52:11 -0400

    Add __init__.py file to QuoteEngine Module

---
+++ QuoteEngine/__init__.py
@@ -0,0 +1,6 @@
+"""Import ingestor helper classes"""
+
+from .CSVIngestor import CSVIngestor
+from .DocxIngestor import DocxIngestor
+from .PDFIngestor import PDFIngestor
+from .txtIngestor import txtIngestor
