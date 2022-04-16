d51cff908917adcea67169bf1eb3d69ffcf48b0e
albergvasili <albergvasili@yandex.com>
2022-04-16 19:56:57 -0400

    Add txt ingestor

---
+++ QuoteEngine/txtIngestor.py
@@ -0,0 +1,30 @@
+"""Ingest txt files."""
+
+from .IngestorInterface import IngestorInterface
+from .QuoteModule import QuoteModule
+from typing import List
+import os
+
+
+class txtIngestor(IngestorInterface):
+    """Inherit from IngestorInterface to convert and ingest txt files."""
+
+    extensions = ['.txt']
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModule]:
+        """Verify if file is a txt, parse and return quotes."""
+        if not cls.can_ingest(path):
+            raise Exception('Cannot ingest Exception')
+
+        quotes = []
+
+        with open(path, 'r') as txt:
+            for line in txt:
+                line = line.strip('\n\r').strip()
+                if len(line) > 0:
+                    parse = line.split('-')
+                    quote = QuoteModel(parse[0], parse[1])
+                    quotes.append(quote)
+
+        return quotes
