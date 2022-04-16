ce832ff165855bb4a02f721abcc4a39d95657239
albergvasili <albergvasili@yandex.com>
2022-04-16 18:35:23 -0400

    Check style with pycodestyle

--- QuoteEngine/IngestorInterface.py
+++ QuoteEngine/IngestorInterface.py
@@ -2,16 +2,16 @@ from .QuoteModule import QuoteModule
 from typing import List
 from abc import ABC, abstractmethod
 
+
 class IngestorInterface(ABC):
-    extensions=[]
+    extensions = []
 
     @classmethod
-    def can_ingest(cls, path:str) -> bool:
+    def can_ingest(cls, path: str) -> bool:
         ext = path.split('.')[-1]
         return ext in extensions
 
     @classmethod
     @abstractmethod
-    def parse(cls, path:str) -> List[QuoteModule}:
+    def parse(cls, path: str) -> List[QuoteModule}:
         pass
-
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-#Meme Generator
+# Meme Generator
 
-##This is a Udacity project from the course 'Intermediate Python'
+This is a Udacity project from the course 'Intermediate Python'
 
-##Project Overview:
+## Project Overview:
 Build a meme generator - a multimedia application to dynamically generate memes, including an image with an overlaid quote.
 
-##Instructions:
+## Instructions:
 
 
 
