a0866eed1f9b1de51058754557ee454aefbea07b
albergvasili <albergvasili@yandex.com>
2022-04-16 21:05:51 -0400

    Import main ingestor in __init__.py file

--- QuoteEngine/__init__.py
+++ QuoteEngine/__init__.py
@@ -1,5 +1,6 @@
 """Import ingestor helper classes"""
 
+from .Ingestor import Ingestor
 from .CSVIngestor import CSVIngestor
 from .DocxIngestor import DocxIngestor
 from .PDFIngestor import PDFIngestor
