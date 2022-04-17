dda31205822fffb5facdba6b5574e0b5a0a03bd9
albergvasili <albergvasili@yandex.com>
2022-04-17 16:49:02 -0400

    Check for bugs and typos

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def meme_rand():
     """Generate a random meme."""
     img = random.choice(imgs)
     quote = random.choice(quotes)
-    path = meme.make_meme(img, quote.body, quote.author)
+    path = meme.generate_meme(img, quote.body, quote.author)
     return render_template('meme.html', path=path)
 
 
