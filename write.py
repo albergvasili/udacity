c6857bd31cd4cbfe6361e66075b1f49c8b1cc39c
albergvasili <albergvasili@yandex.com>
2022-04-26 14:11:48 -0400

    Ouput results to json and csv

--- write.py
+++ write.py
@@ -12,6 +12,7 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
+from helpers import datetime_to_str
 
 
 def write_to_csv(results, filename):
@@ -28,6 +29,21 @@ def write_to_csv(results, filename):
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
+    with open(filename, 'w') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames)
+        writer.writeheader()
+        for elem in results:
+            row = {
+                    'datetime_utc': datetime_to_str(elem.time),
+                    'distance_au': elem.distance, 
+                    'velocity_km_s': elem.velocity,
+                    'designation': elem.designation,
+                    'name': elem.neo.name,
+                    'diameter_km': elem.neo.diameter, 
+                    'potentially_hazardous': elem.neo.hazardous
+            }
+            writer.writerow(row)
+
     # TODO: Write the results to a CSV file, following the specification in the instructions.
 
 
@@ -43,3 +59,20 @@ def write_to_json(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # TODO: Write the results to a JSON file, following the specification in the instructions.
+    with open(filename, 'w') as outfile:
+        obj = []
+        for elem in results:
+            dicts = {
+                'datetime_utc': datetime_to_str(elem.time),
+                'distance_au': elem.distance, 
+                'velocity_km_s': elem.velocity,
+                'neo': {
+                    'designation': elem.designation,
+                    'name': elem.neo.name,
+                    'diameter_km': elem.neo.diameter, 
+                    'potentially_hazardous': elem.neo.hazardous
+                }
+            }
+
+            obj.append(dicts)
+        json.dump(obj, outfile)
