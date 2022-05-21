2cfb642ddb4ec85eb1e6212e77ebbd7303691a24
albergvasili <albergvasili@yandex.com>
2022-05-21 19:50:12 -0400

    Query database to answer questions from part 3

--- queries.sql
+++ queries.sql
@@ -130,3 +130,136 @@ ON t2.region = t1.region
 ORDER BY change;
 
 --Sub-Saharan Africa 30.67 -> 28.78; Latin America & Caribb 51.09 -> 46.16
+
+
+/* Country-level detail */
+
+-- a. Which 5 countries saw the largest amount decrease in forest area from 1990 to 2016? What was the difference in forest area for each?
+
+WITH t1 AS (
+        SELECT country_name, year,
+         SUM(forest_area_sqkm)
+         AS total_forest_area
+        FROM forestation
+        WHERE year = '1990' AND region != 'World'
+        GROUP BY 1, year),
+t2 AS (SELECT country_name, year,
+        SUM(forest_area_sqkm)
+        AS total_forest_area
+       FROM forestation
+       WHERE year = '2016' AND region != 'World'
+       GROUP BY 1, year)
+SELECT t1.country_name,
+       t1.total_forest_area AS forest_area_1990,
+       t2.total_forest_area AS forest_area_2016,
+       t1.total_forest_area - t2.total_forest_area
+       AS forest_area_difference
+FROM t1
+JOIN t2
+ON t2.country_name = t1.country_name
+WHERE t1.total_forest_area IS NOT NULL
+	AND t2.total_forest_area IS NOT NULL
+ORDER BY forest_area_difference DESC
+LIMIT 5;
+
+--Brazil 541510; Indonesia 282193.9844; Myanmar	107234.0039; Nigeria 106506.00098; Tanzania 102320.
+
+-- b. Which 5 countries saw the largest percent decrease in forest area form 1990 to 2016? What was the percent change to 2 decimal places for each?
+
+WITH t1 AS (
+        SELECT country_name, year,
+         SUM(forest_area_sqkm)
+         AS total_forest_area
+        FROM forestation
+        WHERE year = '1990' AND region != 'World'
+        GROUP BY 1, year),
+t2 AS (SELECT country_name, year,
+        SUM(forest_area_sqkm)
+        AS total_forest_area
+       FROM forestation
+       WHERE year = '2016' AND region != 'World'
+       GROUP BY 1, year)
+SELECT t1.country_name,
+       (t1.total_forest_area - t2.total_forest_area)
+        / t1.total_forest_area * 100
+        AS percent_change
+FROM t1
+JOIN t2
+ON t2.country_name = t1.country_name
+WHERE t1.total_forest_area IS NOT NULL
+	AND t2.total_forest_area IS NOT NULL
+ORDER BY 2 DESC
+LIMIT 5;
+
+--Togo 75.44, Nigeria 61.79, Uganda 59.12, Mauritania 46.74, Honduras 45.03.
+
+-- c. If countries were grouped by percent forestation in quartiles, which group had the most countries in it in 2016?
+
+WITH t1 AS (SELECT country_name,
+		percent_forestation,
+            CASE WHEN percent_forestation <= 25 THEN '1st'
+            WHEN percent_forestation <= 50 THEN '2nd'
+            WHEN percent_forestation <= 75 THEN '3rd'
+            ELSE '4th' END AS quartiles
+            FROM (SELECT country_name,
+                   (forest_area_sqkm / total_area_sqkm) * 100
+                   AS percent_forestation
+                  FROM forestation
+                  WHERE year = '2016' AND region != 'World'
+                   AND forest_area_sqkm IS NOT NULL
+                   AND total_area_sqkm IS NOT NULL
+                  GROUP BY 1, 2) AS t2
+            WHERE percent_forestation IS NOT NULL
+            GROUP BY 1, 2)
+SELECT quartiles, COUNT(*)
+FROM t1
+GROUP BY 1
+ORDER BY 1;
+
+--group 1 < 25%
+
+-- d. List all of the countries that were in the 4th quartile (percent forest > 75%) in 2016.
+
+WITH t1 AS (SELECT country_name,
+                percent_forestation,
+            CASE WHEN percent_forestation <= 25 THEN '1st'
+            WHEN percent_forestation <= 50 THEN '2nd'
+            WHEN percent_forestation <= 75 THEN '3rd'
+            ELSE '4th' END AS quartiles
+            FROM (SELECT country_name,
+                   (forest_area_sqkm / total_area_sqkm) * 100
+                   AS percent_forestation
+                  FROM forestation
+                  WHERE year = '2016' AND region != 'World'
+                   AND forest_area_sqkm IS NOT NULL
+                   AND total_area_sqkm IS NOT NULL
+                  GROUP BY 1, 2) AS t2
+            WHERE percent_forestation IS NOT NULL
+            GROUP BY 1, 2)
+SELECT country_name
+FROM t1
+WHERE quartiles = '4th'
+GROUP BY 1
+ORDER BY 1;
+
+--American Samoa, Gabon, Guyana, Lao PDR, Micronesia Fed. Sts., Palau Seychelles, Solomon Islands, Suriname.
+
+-- e. How many countries had a percent forestation higher than the United States in 2016?
+
+SELECT country_name,
+       (forest_area_sqkm / total_area_sqkm) * 100
+       AS percent_forestation
+       FROM forestation
+       WHERE year = '2016'
+        AND region != 'World'
+        AND ((forest_area_sqkm / total_area_sqkm) * 100) > (
+                SELECT (forest_area_sqkm / total_area_sqkm) * 100
+                AS percent_forestation
+                FROM forestation
+                WHERE year = '2016' AND country_name = 'United States'
+)
+        AND forest_area_sqkm IS NOT NULL
+        AND total_area_sqkm IS NOT NULL;
+
+-- 94
+
