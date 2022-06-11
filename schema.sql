14bbdf127d149df727056d0191dfd36e9e657d13
albergvasili <albergvasili@yandex.com>
2022-06-11 13:00:03 -0400

    Constraint foreign keys as NOT NULL

--- schema.sql
+++ schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE users (
 -- Topics:
 CREATE TABLE topics (
         id SERIAL PRIMARY KEY,
-        user_id INTEGER REFERENCES users (id),
+        user_id INTEGER NOT NULL
+          REFERENCES users (id),
         name VARCHAR(30) NOT NULL,
         description VARCHAR(500) DEFAULT NULL,
         CONSTRAINT "unique_name" UNIQUE (name)
@@ -24,9 +25,11 @@ CREATE INDEX "topic_name" ON topics (name);
 -- Posts:
 CREATE TABLE posts (
         id SERIAL,
-        user_id INTEGER REFERENCES users (id) ON DELETE SET NULL, 
+        user_id INTEGER 
+          REFERENCES users (id) ON DELETE SET NULL, 
         title VARCHAR(100) NOT NULL,
-        topic VARCHAR(30) REFERENCES topics (name) ON DELETE CASCADE,
+        topic VARCHAR(30) NOT NULL
+          REFERENCES topics (name) ON DELETE CASCADE,
         url VARCHAR(4000),
         text_content TEXT,
         post_date TIMESTAMP,
@@ -45,8 +48,10 @@ CREATE INDEX "post_by_user" ON posts (user_id);
 -- Comments:
 CREATE TABLE comments (
         id SERIAL,
-        post_id INTEGER REFERENCES posts (id) ON DELETE CASCADE,
-        user_id INTEGER REFERENCES users (id) ON DELETE SET NULL, 
+        post_id INTEGER NOT NULL 
+          REFERENCES posts (id) ON DELETE CASCADE,
+        user_id INTEGER 
+          REFERENCES users (id) ON DELETE SET NULL, 
         text_content TEXT NOT NULL,
         parent_comment INTEGER,
         CONSTRAINT "unique_comments_id" UNIQUE (id)
@@ -61,8 +66,10 @@ CREATE INDEX "parent_comment" ON comments (parent_comment);
 
 -- Votes:
 CREATE TABLE votes (
-        post_id INTEGER REFERENCES posts (id) ON DELETE CASCADE,
-        user_id INTEGER REFERENCES users (id) ON DELETE SET NULL,
+        post_id INTEGER NOT NULL
+          REFERENCES posts (id) ON DELETE CASCADE,
+        user_id INTEGER 
+          REFERENCES users (id) ON DELETE SET NULL,
         value INTEGER,
         CONSTRAINT "unique_vote" UNIQUE (post_id, user_id)
 );
