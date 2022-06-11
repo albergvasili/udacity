a2ef41131855516a5078f3e7a7c489cab681c273
albergvasili <albergvasili@yandex.com>
2022-06-11 13:29:19 -0400

    Import users from up and downvotes columns

--- migrating.sql
+++ migrating.sql
@@ -8,7 +8,18 @@ SELECT DISTINCT username
         UNION
 
 SELECT DISTINCT username
- FROM bad_comments;
+ FROM bad_comments
+        
+        UNION
+
+SELECT DISTINCT REGEXP_SPLIT_TO_TABLE("upvotes", ','),
+FROM bad_posts
+        
+        UNION
+
+SELECT DISTINCT REGEXP_SPLIT_TO_TABLE("downvotes", ',') 
+FROM bad_posts;
+
 
 
 -- Topics:
