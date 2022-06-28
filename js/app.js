e8792fc3e4c5f653ed04a758bfa34696deedb62d
albergvasili <albergvasili@yandex.com>
2022-06-28 17:17:28 -0400

    Add content to nav bar using JavaScript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 /* SCRIPT */
 
+const nav = document.querySelector("#nav-list");
+const fragment = document.createDocumentFragment();
+
+/* Create and add sections to the navigation menu */
+for (let i = 1; i <= 4; i++) {
+        const listItem = document.createElement("li");
+        listItem.innerText = "Section " + i;
+        listItem.classList.add("section-button");
+        fragment.appendChild(listItem);
+}
+
+nav.appendChild(fragment);
+
 
