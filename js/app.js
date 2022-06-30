949358434e08ca6a44300c7a61c13823fdf6c733
albergvasili <albergvasili@yandex.com>
2022-06-30 17:35:04 -0400

    Add anchor link to jump to each section from the nav bar

--- js/app.js
+++ js/app.js
@@ -5,12 +5,29 @@ const fragment = document.createDocumentFragment();
 
 /* Create and add sections to the navigation menu */
 for (let i = 1; i <= 4; i++) {
+
+        let sectionNumber = document.querySelector(`#section${i}`)
+        let sectionTitle = sectionNumber.firstElementChild;
+
         const listItem = document.createElement("li");
-        listItem.innerText = "Section " + i;
         listItem.classList.add("section-button");
+        listItem.setAttribute("id", `nav-section${i}`);
+
+        const anchorElement = document.createElement("a");
+        anchorElement.setAttribute("href", `#section${i}`);
+        anchorElement.innerText = sectionTitle.innerText;
+
+        listItem.appendChild(anchorElement);
+
         fragment.appendChild(listItem);
 }
 
 nav.appendChild(fragment);
 
+
+/* 
+
+scrollTo(coordinates);
+
+*/
 
