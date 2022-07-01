43d08345891d29a0b5426ca8b630c1d27467ca47
albergvasili <albergvasili@yandex.com>
2022-07-01 18:23:51 -0400

    Scroll to sectionby pressing nav bar button

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ const fragment = document.createDocumentFragment();
 
 /* Create and add sections to the navigation menu */
 for (let i = 1; i <= 4; i++) {
-
         let sectionNumber = document.querySelector(`#section${i}`)
         let sectionTitle = sectionNumber.firstElementChild;
 
@@ -20,14 +19,28 @@ for (let i = 1; i <= 4; i++) {
         listItem.appendChild(anchorElement);
 
         fragment.appendChild(listItem);
-}
+};
 
 nav.appendChild(fragment);
 
 
-/* 
+/* Scroll to section by cliking on navigation bar buttons */
+for (let i = 1; i <= 4; i++) {
+        let sectionButton = document.querySelector(`#nav-section${i}`);
+        let sectionNumber = document.querySelector(`#section${i}`)
+        let sectionCoordinates = sectionNumber.getBoundingClientRect();
+        let topOfSection = sectionCoordinates.top;
 
-scrollTo(coordinates);
 
-*/
+        sectionButton.addEventListener("click", function () {
+                scrollTo({
+                        top: topOfSection - 50,
+                        left: 0,
+                        behavior: "smooth"
+                })
+        });               
 
+
+                
+
+};
