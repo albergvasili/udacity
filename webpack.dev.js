41d12c6d46a756f08ddebdd9c88ad7df223c6cbd
albergvasili <albergvasili@yandex.com>
2022-08-24 18:05:52 -0400

    Install plugins Sass and Workbox Service Workers

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,13 +2,23 @@ const path = require("path");
 const webpack = require("webpack");
 module.exports = {
         entry: './src/client/index.js',
+        output: {
+                libraryTarget: 'var',
+                library: 'Client'
+        },
+
         module: {
                 rules: [
                         {
                                 test: /\.js$/,
                                 exclude: /node_modules/,
                                 loader: "babel-loader"
+                        },
+                        {
+                                test: /\.scss$/,
+                                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
                         }
+
                 ]
         }
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,26 @@
 const path = require("path");
 const webpack = require("webpack");
+const WorkboxPlugin = require('workbox-webpack-plugin');
+new WorkboxPlugin.GenerateSW();
+
 module.exports = {
         entry: './src/client/index.js',
+        output: {
+                libraryTarget: 'var',
+                library: 'Client'
+        },
         module: {
                 rules: [
                         {
                                 test: /\.js$/,
                                 exclude: /node_modules/,
                                 loader: "babel-loader"
+                        },
+                        {
+                                test: /\.scss$/,
+                                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
                         }
+
                 ]
         }
 
